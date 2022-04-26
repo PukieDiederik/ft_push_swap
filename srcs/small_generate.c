b8cfaa430391e42b2673993151b1b3c15ade8807
Diederik Robert marie boeren <drobert-@c1r18s5.42lisboa.com>
2022-04-26 16:17:21 +0100

    fixed norm errors on small_generate

--- srcs/small_generate.c
+++ srcs/small_generate.c
@@ -1,51 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_generate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drobert- <drobert-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 16:15:46 by drobert-          #+#    #+#             */
+/*   Updated: 2022/04/26 16:17:03 by drobert-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include "operations.h"
 
+t_stack	*sort_find_smallest(t_stack **a, t_stack **b);
 
-/*
-0 1 2
-0 2 1
-2 1 0
-2 0 1
-1 0 2
-1 2 0
-
-*/
-
-t_stack *sort_find_smallest(t_stack **a, t_stack **b);
-
-void generate_3(t_stack **a)
+void	generate_3(t_stack **a)
 {
-    if ((*a)->value == 1 && (*a)->prev->value == 0)
-        swap_a(a);
-    else if ((*a)->value == 2 && (*a)->prev->value == 0)
-        rotate_a(a);
-    else if ((*a)->value == 1 && (*a)->prev->value == 2)
-        rrotate_a(a);
-    else if ((*a)->value == 2 && (*a)->prev->value == 1)
-    {
-        swap_a(a);
-        rrotate_a(a);
-    }
-    else if ((*a)->value == 0 && (*a)->prev->value == 2)
-    {
-        swap_a(a);
-        rotate_a(a);
-    }
+	if ((*a)->value == 1 && (*a)->prev->value == 0)
+		swap_a(a);
+	else if ((*a)->value == 2 && (*a)->prev->value == 0)
+		rotate_a(a);
+	else if ((*a)->value == 1 && (*a)->prev->value == 2)
+		rrotate_a(a);
+	else if ((*a)->value == 2 && (*a)->prev->value == 1)
+	{
+		swap_a(a);
+		rrotate_a(a);
+	}
+	else if ((*a)->value == 0 && (*a)->prev->value == 2)
+	{
+		swap_a(a);
+		rotate_a(a);
+	}
 }
 
-void generate_5(t_stack **a)
+void	generate_5(t_stack **a)
 {
-	t_stack *b;
-	if((*a)->value == 0 && (*a)->prev->value == 1 && (*a)->prev->prev->value == 2 &&
-		(*a)->next->value == 4 && (*a)->next->next->value == 3)
-		return ;
+	t_stack	*b;
 
+	if ((*a)->value == 0 && (*a)->prev->value == 1
+		&& (*a)->prev->prev->value == 2
+		&& (*a)->next->value == 4 && (*a)->next->next->value == 3)
+		return ;
 	b = 0;
-	if((*a)->value == 0)
+	if ((*a)->value == 0)
 		rotate_a(a);
 	push_b(a, &b);
-	if((*a)->value == 0)
+	if ((*a)->value == 0)
 		rotate_a(a);
 	push_b(a, &b);
 	generate_3(a);
